10881dff25e74929bcf320d5387f556c3756ee17
lichtkind <lichtkind@cpan.org>
2020-02-12 21:59:38 +0100

    adabt scope api

--- dev/overview/lib/Kephra/Base/Class/Scope.pm
+++ dev/overview/lib/Kephra/Base/Class/Scope.pm
@@ -1,17 +1,17 @@
-use v5.16;
+use v5.18;
 use warnings;
 
 # central store for namespace constants
 
 package Kephra::Base::Class::Scope;
 
-sub is   {}  # ~scope                      --> bool
-sub list {}  #                             --> @~scope
-sub list_all {}#                           --> @~scope
+sub is_name            {}  # ~scope          --> bool
+sub method_scope_names {}  #                 --> @~scope
+sub all_scope_names    {}  #                 --> @~scope
 
-sub is_first_tighter{}  # ~scopeA ~scopeB  --> bool
+sub is_first_tighter{}  # ~scopeA ~scopeB    --> bool
 
 sub included_names  {}  # ~scope ~class - ~name ~method  --> @~full_name
-sub name            {}  # ~scope ~class - ~name ~method  --> ~full_name
+sub construct_path  {}  # ~scope ~class - ~name ~method  --> ~full_name
 
 1;
