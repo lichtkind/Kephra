020d08170088dbf04ac2ce3c1909344f49871aad
lichtkind <lichtkind@cpan.org>
2020-02-13 07:28:37 +0100

    found right order of testing module Base Class

--- dev/module/Base-Class/lib/Kephra/Base/Class/Attribute.pm
+++ dev/module/Base-Class/lib/Kephra/Base/Class/Attribute.pm
@@ -6,10 +6,18 @@ use warnings;
 package Kephra::Base::Class::Attribute;
 our $VERSION = 0.04;
 use Kephra::Base::Package;
+use Kephra::Base::Data::Type;
 
 my %value = ();
 
 ################################################################################
+sub create_accessors {
+    my ($class, $attribute, $class_types, $type) = @_;
+    my $callback = $class_types->check_callback($type);
+    my $default = $class_types->default_value($type);
+    return 0 unless ref $callback;
+
+}
 sub create {
     my ($key, $value) = @_;
     return 0 unless @_ < 3;
@@ -25,3 +33,20 @@ sub delete   { delete $value{int $_[0]}  if is_known($_[0]) }
 ################################################################################
 
 1;
+
+__END__
+my $universal_getter = sub{Kephra::Base::Class::Attribute::get($_[0])};
+my (%setter, %resetter);
+
+################################################################################
+sub create_attribute_accessors {
+
+    $resetter{$class}{$type} = sub{set($_[0], $default )} unless ref $resetter{$class}{$type};
+    $setter{$class}{$type} = sub{   $callback->($_[1], Kephra::Base::Class::Instance::get_access_self($_[0]))
+                         or Kephra::Base::Class::Instance::Attribute::set(@_[0,1])} unless ref $setter{$class}{$type};
+
+    Kephra::Base::Package::set_sub( Kephra::Base::Class::Scope::name('attribute',$class,$attribute,'get'), $universal_getter);
+    Kephra::Base::Package::set_sub( Kephra::Base::Class::Scope::name('attribute',$class,$attribute,'set'),  $setter{$type});
+    Kephra::Base::Package::set_sub( Kephra::Base::Class::Scope::name('attribute',$class,$attribute,'reset'), $resetter{$type});
+    1;
+}
--- dev/module/Base-Class/lib/Kephra/Base/Class/Scope.pm
+++ dev/module/Base-Class/lib/Kephra/Base/Class/Scope.pm
@@ -2,19 +2,24 @@ use v5.18;
 use warnings;
 
 # central store for method namespace constants and their priority logic
+# Classname::method
+# Classname::PRIVATE::methodname          # & ACCESS/BUILD
+# Classname::HOOK::method::BEFORE/AFTER
+# Classname::ARGUMENT::method::arghashref
+# Classname::ATTRIBUTE::attrname::get/set/reset
 
 package Kephra::Base::Class::Scope;
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 ################################################################################
 my %constant = (hook => 'HOOK', argument => 'ARGUMENT',attribute => 'ATTRIBUTE',
                 public => '',   private => 'PRIVATE',  access => 'ACCESS', build => 'BUILD');
 my %level    = (public => 1,    private => 2,          access => 3,        build => 4);
 my @importance = sort {$level{$b} <=> $level{$a}} (keys %level);
 ################################################################################
-sub list_method_names { @importance }
-sub list_all_names    { keys %constant }
-sub is_name           { $_[0] and exists $level{$_[0]} }
-sub is_first_tighter  {
+sub method_scope_names { @importance }
+sub all_scope_names    { keys %constant }
+sub is_name            { $_[0] and exists $level{$_[0]} }
+sub is_first_tighter   {
     return undef unless @_ == 2 and $level{$_[0]} and $level{$_[1]};
     $level{$_[0]} >= $level{$_[1]};
 }
