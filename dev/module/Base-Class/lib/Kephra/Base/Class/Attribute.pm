e4ced58c451729daa870211d84d9bbe981329d6f
Lichtkind <lichtkind@cpan.org>
2020-02-17 19:40:46 +0100

    harden the attr api

--- dev/module/Base-Class/lib/Kephra/Base/Class/Attribute.pm
+++ dev/module/Base-Class/lib/Kephra/Base/Class/Attribute.pm
@@ -9,93 +9,105 @@ use Kephra::Base::Package qw/set_sub has_sub/;
 use Kephra::Base::Class::Scope qw/cat_scope_path/;
 use Kephra::Base::Class::Attribute::Type;
 
-my (%store, %setter, %resetter, %tr_getter, %tr_setter, %tr_getsetter);
-my $universal_getter = sub{$store{int $_[0]} if ref $_[0] and exists $store{int $_[0]} };
+my (%data, %attr_name, %setter, %resetter, %attr_ref_getter, %attr_ref_setter, %attr_ref_getsetter, %self_ref);
+my $universal_getter = sub{$data{int $_[0]} if ref $_[0] and exists $data{int $_[0]} };
 
 ################################################################################
 sub create {
     my ($class, $attribute, $attr_types, $type) = @_;
     return 0 unless ref $attr_types eq 'Kephra::Base::Class::Attribute::Type' and defined $type;
     my $callback = $attr_types->get_callback($type);
-    my $default = $attr_types->get_default_value($type);
+    my $default_value = $attr_types->get_default_value($type);
     return 0 unless ref $callback eq 'CODE';
 
     my $scope = cat_scope_path( 'attribute', $class, $attribute);
     my $k = '';
-    my $self = bless \$k, $scope;
-    $store{int $self} = $default if defined $default;
+    my $attr_ref = bless \$k, $scope;
+    $data{int $attr_ref} = $default_value if defined $default_value;
+    $attr_name{int $attr_ref} = $attribute;
 
     $setter{$class}{$type} = sub { 
-        return unless ref $_[0] and exists $store{int $_[0]} and defined $_[1];
+        return unless ref $_[0] and exists $data{int $_[0]} and defined $_[1];
         my $a = shift;
         $k = $callback->(@_);
         return if $k;
-        $store{int $a} = $_[0];
+        $data{int $a} = $_[0];
     } unless ref $setter{$class}{$type};
 
     $resetter{$class}{$type} = sub { 
-        return unless ref $_[0] and exists $store{int $_[0]};
-        $store{int $_[0]} = $default;
-        $default;
+        return unless ref $_[0] and exists $data{int $_[0]};
+        $data{int $_[0]} = $default_value;
     } unless ref $resetter{$class}{$type};
 
     set_sub( $scope.'::get', $universal_getter);
     set_sub( $scope.'::set', $setter{$class}{$type});
     set_sub( $scope.'::reset', $resetter{$class}{$type});
-    $self;
+    $attr_ref;
 }
 
 sub add_getter {
-    my ($attribute, $attr_ref, $path, $self) = @_;
-    return 0 unless ref $attr_ref and exists $store{int $attr_ref} and ref $self;
-    $tr_getter{int $self}{$attribute} = $attr_ref;
+    my ($attr_ref, $path, $self) = @_;
+    return 0 unless ref $attr_ref and exists $data{int $attr_ref} and ref $self;
+    my $attribute = $attr_name{int $attr_ref};
+    $attr_ref_getter{int $self}{$attribute} = $attr_ref;
+    $self_ref{int $attr_ref}{int $self}++;
     set_sub( $path, sub {
-        return unless ref $_[0] and ref $tr_getter{int $_[0]};
-        $store{int $tr_getter{int $_[0]}{$attribute}};
+        return unless ref $_[0] and ref $attr_ref_getter{int $_[0]}{$attribute};
+        $data{int $attr_ref_getter{int $_[0]}{$attribute}};
     });
     1;
 } 
 sub add_setter {
-    my ($attribute, $attr_ref, $path, $self) = @_;
-    return 0 unless ref $attr_ref and exists $store{int $attr_ref} and ref $self;
-    $tr_setter{int $self}{$attribute} = $attr_ref;
+    my ($attr_ref, $path, $self) = @_;
+    return 0 unless ref $attr_ref and exists $data{int $attr_ref} and ref $self;
+    my $attribute = $attr_name{int $attr_ref};
+    $attr_ref_setter{int $self}{$attribute} = $attr_ref;
+    $self_ref{int $attr_ref}{int $self}++;
     set_sub( $path, sub {
-        return unless ref $_[0] and ref $tr_setter{int $_[0]} and defined $_[1];
+        return unless ref $_[0] and ref $attr_ref_setter{int $_[0]}{$attribute} and defined $_[1];
         my $self = shift;
-        my $attr = $tr_getter{int $self}{$attribute};
-        my $ret = $attr->set($_[0], $self); # get return value
-        $$self = $$attr;    # copy error msg
+        my $attr_ref = $attr_ref_setter{int $self}{$attribute};
+        my $ret = $attr_ref->set($_[0], $self); # get return value
+        $$self = $$attr_ref;    # copy error msg
         $ret;
     });
     1;
 }
 sub add_getsetter {
-    my ($attribute, $attr_ref, $path, $self) = @_;
-    return 0 unless ref $attr_ref and exists $store{int $attr_ref} and ref $self;
-    $tr_getsetter{int $self}{$attribute} = $attr_ref;
+    my ($attr_ref, $path, $self) = @_;
+    return 0 unless ref $attr_ref and exists $data{int $attr_ref} and ref $self;
+    my $attribute = $attr_name{int $attr_ref};
+    $attr_ref_getsetter{int $self}{$attribute} = $attr_ref;
+    $self_ref{int $attr_ref}{int $self}++;
     set_sub( $path, sub {
-        return unless ref $_[0] and ref $tr_getsetter{int $_[0]};
+        return unless ref $_[0] and ref $attr_ref_getsetter{int $_[0]}{$attribute};
         my $self = shift;
-        my $attr = $tr_getsetter{int $self}{$attribute};
-        return $attr->get() unless defined $_[0];
-        my $ret = $attr->set($_[0], $self);
-        $$self = $$attr; # copy error msg
+        my $attr_ref = $attr_ref_getsetter{int $self}{$attribute};
+        return $data{int $attr_ref} unless defined $_[0];
+        my $ret = $attr_ref->set($_[0], $self);
+        $$self = $$attr_ref; # copy error msg
         $ret;
     });
     1;
 }
 
 sub delete   { # autodelete translations?
-    my $attr = shift;
-    delete $tr_getter{int $_} for @_;
-    delete $tr_setter{int $_} for @_;
-    delete $tr_getsetter{int $_} for @_;
-    delete $store{int $attr} if is_known($attr);
+    my $attr_ref = shift;
+    return 0 unless ref $attr_ref and exists $data{int $attr_ref};
+    my $attribute = $attr_name{int $attr_ref};
+    delete $attr_ref_getter{$_}{$attribute} for keys %{$self_ref{int $attr_ref}};
+    delete $attr_ref_setter{$_}{$attribute}  for keys %{$self_ref{int $attr_ref}};
+    delete $attr_ref_getsetter{$_}{$attribute} for keys %{$self_ref{int $attr_ref}};
+    delete $self_ref{int $attr_ref};
+    delete $attr_name{int $attr_ref};
+    delete $data{int $attr_ref};
 }
+
+sub remove_class {}
 ################################################################################
-sub is_known { (ref $_[0] and exists $store{int $_[0]}) ? 1 : 0 }
-sub get      { $store{int $_[0]}         if is_known($_[0]) }
-sub set      { $store{int $_[0]} = $_[1] if is_known($_[0]) }
+sub is_known { (ref $_[0] and exists $data{int $_[0]}) ? 1 : 0 }
+sub get      { $data{int $_[0]}         if is_known($_[0]) }
+sub set      { $data{int $_[0]} = $_[1] if is_known($_[0]) }
 ################################################################################
 
 1;
