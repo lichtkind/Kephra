aff20d44c74ed4cb5b606dedca6668f1b7180897
Lichtkind <lichtkind@cpan.org>
2020-03-08 02:03:08 +0100

    alo changed call api

--- dev/overview/lib/Kephra/Base/Call.pm
+++ dev/overview/lib/Kephra/Base/Call.pm
@@ -3,14 +3,15 @@ use warnings;
 
 package  Kephra::Base::Call;
 
-sub new        {} # ~source - $state  --> .call|~evalerror   # call with own source and state
-                  # .call   - $state  --> .call              # another call with same source
-sub clone      {} # .call             --> .call              # another call in same source and same state
+sub new        {} # ~source - $state ~set_type ~get_type --> .call|~evalerror|~error   # create call object with own source and state
+sub clone      {} # .call                                --> .call                     # create call object with same source and types and same (or different) state
 
 sub get_source {} # .call             --> ~source
-sub get_state  {} # .call             --> $state
-sub set_state  {} # .call $data       --> $state
+sub get_gettype{} # .call             --> ~set_type    # ~~ Kephra::Base::Data::Type 
+sub get_settype{} # .call             --> ~get_type    # ~~ Kephra::Base::Data::Type
+sub get_state  {} # .call             --> $state       # get $state if $state is of ~get_type
+sub set_state  {} # .call $data       --> $state       # change $state if $data is of ~set_type
 
-sub run        {} #         : @arg    --> $retval
+sub run        {} # .call   - @arg    --> $retval      # run evaled source
 
 1;
