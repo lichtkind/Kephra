b021a5fba095b0834a5b2e938eed783877838fa8
Lichtkind <lichtkind@cpan.org>
2020-05-08 12:13:40 +0200

    fixed base tests

--- dev/module/Base-Class/lib/Kephra/Base.pm
+++ dev/module/Base-Class/lib/Kephra/Base.pm
@@ -1,17 +1,17 @@
 use v5.20;
 use warnings;
-use Kephra::Base::Package;
-use Kephra::Base::Data qw/:all/;
-use Kephra::Base::Closure qw/:all/;
 
 package Kephra::Base;
 our $VERSION = 0.11;
 use Exporter 'import';
-our @EXPORT_OK = qw/new_counter new_closure/;
+our @EXPORT_OK = qw/new_counter date_time new_closure/;
 our %EXPORT_TAGS = (all  => [@EXPORT_OK]);
 
+use Kephra::Base::Package;
+use Kephra::Base::Data qw/:all/;
+use Kephra::Base::Closure qw/new_closure/;
+use Kephra::Base::Class;
 
-sub new_counter { new_closure('++$state', 0) }
-
+sub new_counter { new_closure('$state++', 0) }
 
 'love and light';
