e7769d82c3abd0b1c7f69ef931cbb595de8cc179
Lichtkind <lichtkind@cpan.org>
2020-05-09 13:53:30 +0200

    rebuild full functionality of func proto packageloader

--- dev/function/bin/oo/packageloader.pl
+++ dev/function/bin/oo/packageloader.pl
@@ -3,10 +3,12 @@ no strict 'refs';
 
 BEGIN { unshift @INC, '.' }
 
+# loads package of gien name and displays methods
+
 my $pkg = shift // 'Base';
 $pkg = substr($pkg,0,-3) if substr($pkg, -3) eq '.pm';
 require "$pkg.pm";
 
 #say join(',', sort keys *{"$pkg"."::"}{HASH});
 no strict 'refs';
-say *{"Base::new"}{CODE};
+say for sort keys %{*{$pkg."::"}{HASH}};
