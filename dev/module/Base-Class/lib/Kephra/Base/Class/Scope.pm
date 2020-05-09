bfb52130b571d7768a6a1b15a88cc8c194256412
Lichtkind <lichtkind@cpan.org>
2020-05-09 13:31:17 +0200

    cimplify base class scoping

--- dev/module/Base-Class/lib/Kephra/Base/Class/Scope.pm
+++ dev/module/Base-Class/lib/Kephra/Base/Class/Scope.pm
@@ -5,49 +5,37 @@ use experimental qw/switch/;
 # method namespace constants and their priority logic
 
 package Kephra::Base::Class::Scope;
-our $VERSION = 1.0;
-use Exporter 'import';
-our @EXPORT_OK = qw/cat_scope_path/;
-our %EXPORT_TAGS = (all  => [@EXPORT_OK]);
+our $VERSION = 1.5;
+
 ################################################################################
 my $prefix   = '-';
-my %name     = (hook => 'HOOK', argument => 'ARGUMENT',attribute => 'ATTRIBUTE',
+my %name     = (attribute => 'ATTRIBUTE', method => 'METHOD', hook => 'HOOK', argument => 'ARGUMENT',
                 public => '',   private => 'PRIVATE',  access => 'ACCESS', build => 'BUILD');
 my %priority = (public => 1,    private => 2,          access => 3,        build => 4); # 
-my @importance = sort {$priority{$b} <=> $priority{$a}} (keys %priority);
-my @names      = values %name;
+my @method_scopes = map { $name{$_} } sort {$priority{$b} <=> $priority{$a}} (keys %priority);
+my @names         = values %name;
 ################################################################################
-sub method_scopes       { @importance }
-sub all_scopes          { keys %name }
-
-sub is_scope            { $_[0] and exists $name{$_[0]} }
-sub is_method_scope     { $_[0] and exists $priority{$_[0]} }
-sub is_name             { $_[0] and $_[0] ~~ \@names }
-
+#sub is_scope            { $_[0] and exists $name{$_[0]} }
+#sub is_method_scope     { $_[0] and exists $priority{$_[0]} }
+#sub is_name             { $_[0] and $_[0] ~~ \@names }
 sub is_first_tighter    { # compare priority of two method scopes
     return undef unless @_ == 2 and $priority{$_[0]} and $priority{$_[1]};
     $priority{$_[0]} >= $priority{$_[1]};
 }
 ################################################################################
-sub included_names {
-    return unless @_ > 1 and $priority{$_[0]};
-    my $scope = shift;
-    my @names;
-    for my $s (@importance){
-        push @names, cat_scope_path($s, @_);
-        return @names if $s eq $scope;
+sub cat_method_paths {
+    return unless @_ == 3 and $priority{$_[0]};
+    my ($scope, $class, $method) = @_;
+    my @pathes;
+    $class .= '::';
+    for my $s (@method_scopes){
+         push @pathes, ($s ? "$class$prefix::$s::" : $class) . $method;
+         return @pathes if $s eq $name{ $scope };
     }
 }
-sub cat_scope_path { # create package name for scope of that class
-    return unless @_ > 1 and defined $name{$_[0]} and not ($_[0] ~~ \@names);
-    my $scope = shift;
-    my $c = shift;
-    # return if defined $_[0] and $_[0] ~~ \@names; # with prefix there are no method name collisions
-    # return if defined $_[1] and $_[1] ~~ \@names;
-    $c .= '::'.$prefix.'::'.$name{$scope} if $name{$scope};
-    $c .= '::'.$_[0] if $_[0];
-    $c .= '::'.$_[1] if $_[1] and not $priority{ $scope };
-    $c;
-}
+sub cat_hook_path      {return unless @_ == 2; "$_[0]::$prefix::METHOD::$_[1]::HOOK" }
+sub cat_argument_path  {return unless @_ == 2; "$_[0]::$prefix::METHOD::$_[1]::ARGUMENT" }
+sub cat_attribute_path {return unless @_ == 2; "$_[0]::$prefix::ATTRIBUTE::$_[1]" }
 ################################################################################
+
 1;
