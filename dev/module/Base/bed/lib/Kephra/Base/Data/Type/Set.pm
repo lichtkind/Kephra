62ee3415bf5a0604a3c462c1bff2763d4ca883ac
Lichtkind <lichtkind@cpan.org>
2022-08-07 10:52:39 +0200

    refine type set API

--- dev/module/Base/bed/lib/Kephra/Base/Data/Type/Set.pm
+++ dev/module/Base/bed/lib/Kephra/Base/Data/Type/Set.pm
@@ -6,7 +6,7 @@ use warnings;
 #       open sets ($self->{open} eq 'open') cannot be closed (like normal == 0 | 1 = ?) 
 
 package Kephra::Base::Data::Type::Set;
-our $VERSION = 0.21;
+our $VERSION = 0.3;
 use Kephra::Base::Data::Type::Factory ':all';
 
 #### constructor, serialisation ################################################
@@ -266,32 +266,36 @@ sub _check_symbol {
     return "$kind type symbol '$symbol' is already taked" if exists $self->{$kind.'_symbol_name'}{$symbol};
     '';
 }
-sub _is_symbol {length $_[0] == 1 and $_[0] !~ /[a-z0-9_]/ }
+
+sub is_symbol {length $_[1] == 1 and $_[1] !~ /[a-z0-9_]/ }  # _ ~symbol  --> ?
 
 5;
 
 __END__
-vsub new                      {} #   -- 'open'                        --> ._       open store can not finalized
+
+sub new                      {} #   -- 'open'                        --> ._       open store can not finalized
 sub state                    {} # _                                  --> %state   dump all active types data
 sub restate                  {} # %state                             --> ._       recreate all type checker from data dump
 
 sub is_open                  {} # _                                  --> ?
 sub close                    {} # _                                  --> ?
 
-sub list_type_names          {} # _  - ~kind ~param_type             --> @~btype|@~ptype|@~param   # ~name     == a-z,(a-z0-9_)*
-sub list_shortcuts           {} # _  - ~kind                         --> @~shortcut                # ~shortcut == [^a-z0-9_]
-sub list_forbidden_shortcuts {} # _                                  --> @~shortcut
 
-sub add_type                 {} # _ .type|%typedef - ~shortcut       --> ~errormsg
+sub add_type                 {} # _ .type|%typedef - ~symbol         --> ~errormsg
 sub remove_type              {} # _ ~type - ~param                   --> .type|~errormsg
 sub get_type                 {} # _ ~type - ~param                   --> .type|~errormsg
+sub list_type_names          {} # _  - ~kind ~param_type             --> @~btype|@~ptype|@~param # ~name     == a-z,(a-z0-9_)*
  
 sub is_type_known            {} # _ ~type - ~param                   --> ?
 sub is_type_owned            {} # _ ~type - ~param                   --> ?
 
-sub add_shortcut             {} # _ ~kind ~type ~shortcut            --> ~errormsg
-sub remove_shortcut          {} # _ ~kind ~shortcut                  --> ~errormsg
-sub get_shortcut             {} # _ ~kind ~type                      --> ~shortcut|~errormsg       # ~kind = 'simple'|'para[meter]'
-sub resolve_shortcut         {} # _ ~kind ~shortcut                  --> ~full_name|undef
-sub allow_shortcuts          {} # _ @~shortcut                       --> @~shortcut                # already allowed shortcuts
-sub forbid_shortcuts         {} # _ @~shortcut                       --> @~shortcut                # already forbidden shortcuts
+
+sub add_symbol               {} # _ ~kind ~type ~symbol              --> ~errormsg
+sub remove_symbol            {} # _ ~kind ~symbol                    --> ~errormsg
+sub get_symbol               {} # _ ~kind ~type                      --> ~symbol|~errormsg       # ~kind = 'simple'|'para[meter]'
+sub resolve_symbol           {} # _ ~kind ~symbol                    --> ~full_name|undef
+
+sub list_symbols             {} # _  - ~kind                         --> @~symbol                # ~shortcut == [^a-z0-9_]
+sub list_forbidden_symbols   {} # _                                  --> @~symbol
+sub allow_symbols            {} # _ @~symbol                         --> @~symbol                # already allowed shortcuts
+sub forbid_symbols           {} # _ @~symbol                         --> @~symbol                # already forbidden shortcuts
